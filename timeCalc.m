6b21b9edb93206cffae2d4ec0f8f4358445c63a3
Willy Chang <willychang@rocketmail.com>
2020-04-24 15:57:51 -1000

    added comments & waiting bar

--- timeCalc.m
+++ timeCalc.m
@@ -1,10 +1,10 @@
 function tB1 = timeCalc(imagename)
+% Determine region of interest on the images
 roi = [435 472 45 15]; 
 A = '0123456789';
 M = [];
 
-%I = imread(imagename);
-% I = imcrop(I,roi);
+% Modify image to make it clearer
 I = imcrop(imagename,roi);
 I = imsharpen(I);
 I = imbinarize(I);
@@ -12,8 +12,12 @@ I = imresize(I,1.3);
 I = bwmorph(I, 'thicken', 3);
 I = bwmorph(I, 'bothat', 1);
 I = bwmorph(I, 'tophat', 1);
+
+% Analyze characters
 results = ocr(I,'CharacterSet', '.0123456789h', 'TextLayout','Block');
 m = results.Text;
+
+% Convert the characters into numbers
 for r = 1:length(m)
     if ismember(m(1,r),A)
         M = [M m(1,r)];
